3dfbb3e852af7d558e440b0081d0b384a5de025a
Katerina <katyasemenova96@gmail.com>
2018-05-30 14:45:22 +0300

    first homework 2nd try

--- src/test/java/hw1/HomePageTest.java
+++ src/test/java/hw1/HomePageTest.java
@@ -15,11 +15,14 @@ import org.testng.Assert;
 import static org.testng.Assert.*;
 
 /**
- * Created by Екатерина on 23.05.2018.
+ * Created by Ekaterina on 23.05.2018.
+ *
  */
 public class HomePageTest {
+    private String title = "Home Page";
+
     @Test
-    public void loginTest() {
+    public void shouldPerformLoggingInCorrectly() {
         System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
         HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
         chromePrefs.put("download.default_directory", "target");
@@ -28,19 +31,17 @@ public class HomePageTest {
         options.setExperimentalOption("prefs", chromePrefs);
 
         WebDriver driver = new ChromeDriver();
-        WebDriver.Window window = driver.manage().window();
-        window.maximize();
+        driver.manage().window().maximize();
 
         //2 Open test site by URL
         driver.navigate().to("https://epam.github.io/JDI/");
 
 
         //3 Assert Browser title
-        checkTitle(driver, "Home Page");
+        checkTitle(driver, title);
 
         //4 Perform login
-        WebElement userIcon = driver.findElement(By.cssSelector(".profile-photo"));
-        userIcon.click();
+        driver.findElement(By.cssSelector(".profile-photo")).click();
         driver.findElement(By.cssSelector("#Name")).sendKeys("epam");
         driver.findElement(By.cssSelector("#Password")).sendKeys("1234");
         driver.findElement(By.cssSelector(".form-horizontal button[type = 'submit']")).click();
@@ -50,21 +51,19 @@ public class HomePageTest {
         assertEquals(userName.getText(), "PITER CHAILOVSKII");
 
         //6 Assert Browser title
-        checkTitle(driver, "Home Page");
+        checkTitle(driver, title);
 
         //7 Assert that there are 4 items on the header section are displayed and they have proper texts
-        WebElement navigateBar = driver.findElement(By.cssSelector(".uui-navigation"));
-        WebElement element = navigateBar.findElement(By.cssSelector("[href='index.html']"));
-        assertEquals(element.getText(), "HOME");
-        element = navigateBar.findElement(By.cssSelector("[href='contacts.html']"));
-        assertEquals(element.getText(), "CONTACT FORM");
-        element = navigateBar.findElement(By.cssSelector("[class='dropdown']"));
-        assertEquals(element.getText(), "SERVICE");
-        element = navigateBar.findElement(By.cssSelector("[href='metals-colors.html']"));
-        assertEquals(element.getText(), "METALS & COLORS");
+        List<WebElement> navigateBarElements = driver.findElements(By.cssSelector(".nav > li"));
+        Assert.assertEquals(navigateBarElements.size(), 4);
+        assertEquals(navigateBarElements.get(0).getText(), "HOME");
+        assertEquals(navigateBarElements.get(1).getText(), "CONTACT FORM");
+        assertEquals(navigateBarElements.get(2).getText(), "SERVICE");
+        assertEquals(navigateBarElements.get(3).getText(), "METALS & COLORS");
 
         //8 Assert that there are 4 images on the Index Page and they are displayed
         List<WebElement> images = driver.findElements(By.cssSelector(".icons-benefit"));
+        Assert.assertEquals(images.size(), 4);
         for (WebElement image : images) {
             assertTrue(image.isDisplayed());
         }
@@ -72,6 +71,7 @@ public class HomePageTest {
         //9 Assert that there are 4 texts on the Index Page under icons and they have proper text
 
         List<WebElement> texts = driver.findElements(By.cssSelector(".benefit-txt"));
+        Assert.assertEquals(texts.size(), 4);
         for (WebElement text : texts) {
             assertTrue(text.isDisplayed());
         }
@@ -87,13 +87,19 @@ public class HomePageTest {
                 "wish to get more…");
 
         //10 Assert a text of the main header
-        // & 11 Assert a text of the sub header
         List<WebElement> centerTexts = driver.findElements(By.cssSelector(".text-center"));
         for (WebElement text : centerTexts) {
             assertTrue(text.isDisplayed());
         }
+        //11 Assert a text of the sub header
         assertEquals(centerTexts.get(0).getText(), "EPAM FRAMEWORK WISHES…");
-        assertEquals(centerTexts.get(1).getText(), "LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISICING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO CONSEQUAT DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR.");
+        assertEquals(centerTexts.get(1).getText(),
+                "LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISICING ELIT, " +
+                "SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA. " +
+                "UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS " +
+                "NISI UT ALIQUIP EX EA COMMODO CONSEQUAT DUIS AUTE IRURE DOLOR IN " +
+                "REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR."
+        );
         assertEquals(centerTexts.get(2).getText(), "JDI GITHUB");
 
         //12 Assert that JDI GITHUB is a link and has a proper URL
@@ -105,11 +111,12 @@ public class HomePageTest {
 
         //14 Assert that there is Footer.
         assertTrue(driver.findElement(By.cssSelector(".footer-content")).isDisplayed());
+
         //15 Close Browser
         driver.close();
     }
 
-    public void checkTitle(WebDriver driver, String title) {
+    private void checkTitle(WebDriver driver, String title) {
         assertEquals(driver.getTitle(), title);
     }
 }
